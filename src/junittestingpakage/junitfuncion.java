dde61d4fb99f7fa23247565e422160f9e22beb72
nihar <nihar@10.12.16.79>
2022-04-19 09:35:23 +0530

    Junittesting

--- src/junittestingpakage/junitfuncion.java
+++ src/junittestingpakage/junitfuncion.java
@@ -4,5 +4,7 @@ public class junitfuncion {
 	public int subnum(int num1,int num2) {
 		return num1-num2;
 	}
-    public 
+    public String addstrings(String s1, String s2) {
+    	return s1+s2;
+    }
 }
